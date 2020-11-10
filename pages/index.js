e02d0d62631cad71aa92f82326018c34109f91d0
JhonnathaMonteiro <jhonnatha.am@gmail.com>
2020-11-10 15:26:29 -0300

    testing commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  useEffect(() => {
+    const callRegister = async () => {
+      axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
+      const response = await axios({
+        method: 'post',
+        url: 'https://hash.immunie.org/hash-register',
+        data: {
+          userData: "USERDAsTA",
+          userVaccines: ["AA", "BBC"],
+          lastAppliedDate: Date.now().toString(),
+        }
+      });
+      console.log("THE RESPONSE FROM BLCK:" , response.data);
+    };
+
+    callRegister();
+
+  }, [])
+
+
   return (
     <div>
       Home
